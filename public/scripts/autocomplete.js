fbfbd6b3025bc478bb4363c774b8ce32044af478
keith <obrienkeithd@gmail.com>
2013-12-06 23:17:30 -0500

    better examples

--- public/scripts/autocomplete.js
+++ public/scripts/autocomplete.js
@@ -7,30 +7,36 @@ directive('ngAutocomplete', function(){
 			retrievalMethod: '&'
 		},
 		link: function(scope, something, attrs){
-			scope.$watch('ngModel', function(oldVal, newVal) {
-				if(!(oldVal == undefined && newVal == undefined)){
-					scope.retrievalMethod()(scope.ngModel)
+			scope.$watch('ngModel', function(newVal, oldVal) {
+				if(!(oldVal == undefined && newVal == undefined) && newVal !== ""){
+					scope.retrievalMethod()(scope.ngModel, max_results)
 					.then(function(data){
 						scope.items = data;
 					});
+				}else{
+					scope.items = [];
 				}
 			});
 
-			var valueProperty = attrs.valueproperty;
+			var value_property = attrs.valueProperty;
+			var advanced_display = attrs.advancedDisplay;
+			var max_results = attrs.maxResults;
 
 			scope.itemSelected = function(item){
 				scope.onSelect()(item);
 			}
 
 			scope.getItemTemplate = function(item){
-				if(valueProperty != undefined){
-					return item[valueProperty];
+				if(value_property != undefined){
+					return item[value_property];
+				}else if(advanced_display != undefined){
+					return eval(advanced_display);
 				}else{
 					return item;
 				}
 			};
 		},
-		template: '<div><input type="text" ng-model="ngModel" />' +
-		'<div ng-click="itemSelected(item)" ng-repeat="item in items">{{getItemTemplate(item)}}</div>'
+		template: '<div class="ac-wrapper"><input type="text" ng-model="ngModel" />' +
+		'<div class="ac-items"><div class="ac-item" ng-click="itemSelected(item)" ng-repeat="item in items">{{getItemTemplate(item)}}</div>'
 	};	
 });
